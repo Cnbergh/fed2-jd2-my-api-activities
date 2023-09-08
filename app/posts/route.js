1a852474dbe87dfa593697cdfe19799bb071c24c
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2023-09-08 10:52:58 +0200

    faker

--- app/posts/route.js
+++ app/posts/route.js
@@ -1,5 +1,19 @@
 import { NextResponse } from "next/server";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    id: faker.string.uuid(),
+    title: faker.lorem.sentence(),
+    body: faker.lorem.paragraph(),
+    author: faker.person.firstName(),
+  };
+}
 
 export async function GET(request) {
-  return NextResponse.json({ posts: "Success" }, { status: 200 });
+  const fakerPosts = faker.helpers.multiple(createRandomPost, {
+    count: 10,
+  });
+
+  return NextResponse.json({ posts: fakerPosts() }, { status: 200 });
 }

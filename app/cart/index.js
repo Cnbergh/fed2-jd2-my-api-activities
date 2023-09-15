c7a8d6c43c83a3d642940ac14080affc8d8bb807
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2023-09-15 10:14:04 +0200

    object entries

--- app/cart/index.js
+++ app/cart/index.js
@@ -20,3 +20,28 @@ addToCart(mosCornerCart, cookies);
 console.log(mosCornerCart);
 
 displayItems(mosCornerCart);
+
+//15.09.23
+const recipe = {
+  ingredient_0: "egg",
+  ingredient_1: "milk",
+  ingredient_2: "flour",
+};
+
+for (let index = 0; index <= 2; index++) {
+  const ingredient = recipe["ingredient_" + index];
+  console.log(ingredient);
+
+  for (const item in recipe) {
+    console.log("using for in >>>", `${item}: ${recipe[item]}`);
+  }
+  console.log(Object.keys(recipe));
+
+  Object.keys(recipe).map((key) => {
+    console.log("using map >>>", recipe[key]);
+  });
+
+  Object.entries(recipe).map(([key, value]) => {
+    console.warn(`using entries >>> key: ${key}, value: ${value}`);
+  });
+}
